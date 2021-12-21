3ce25157bdd79c8e5466520c6fad27447deb667d
Matthew Ceroni <matthew.ceroni@dat.com>
2021-12-21 08:52:01 -0800

    feature: add output EKS cluster name

--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
+output "cluster_name"     { value = aws_eks_cluster.this.name }
 output "cluster_id"       { value = aws_eks_cluster.this.id }
 output "cluster_endpoint" { value = aws_eks_cluster.this.endpoint }
 output "cluster_version"  { value = aws_eks_cluster.this.version }
